6d8a18bf50fd03e1bbfba5362d9a557e7b9a3c05
MarvisClause <slarchienko19@gmail.com>
2022-05-19 15:25:35 +0300

    Added the ability to save the result

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.Win32;
+using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace DisEn
 {
@@ -15,10 +19,10 @@ namespace DisEn
             _disassembler = new Disassembler();
         }
 
-        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        private void BtnOpenFile_Click(object sender, RoutedEventArgs e)
         {
-            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
-            openFileDialog.Filter = "Text files (*.txt)|*.txt|(*.exe)|*.exe|All files (*.*)|*.*";
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "All files (*.*)|*.*";
             if (openFileDialog.ShowDialog() == true)
             {
                 // Disassemble given file
@@ -30,6 +34,29 @@ namespace DisEn
                 // Get commands info
                 List<DissamblerCommandInfo> commandInfos = _disassembler.GetCommandsInfo();
                 DisassemblerDataGrid.ItemsSource = commandInfos;
+            }
+        }
+        private void BtnSaveFile_Click(object sender, System.EventArgs e)
+        {
+            SaveFileDialog saveData = new SaveFileDialog();
+            saveData.Filter = "Текст (*.txt)|*.txt";
+            if (saveData.ShowDialog() == true)
+            {
+                using (StreamWriter sw = new StreamWriter(saveData.OpenFile(), Encoding.UTF8))
+                {
+                    sw.Write("Path to file: " +PathToFileTextBlock.Text); 
+                    sw.Write("\nTotal amout of commands: " + TotalNumOfCommandsTextBlock.Text);
+
+                    DisassemblerDataGrid.SelectAllCells();
+                    DisassemblerDataGrid.ClipboardCopyMode = DataGridClipboardCopyMode.ExcludeHeader;
+                    ApplicationCommands.Copy.Execute(null, DisassemblerDataGrid);
+                    DisassemblerDataGrid.UnselectAllCells();
+                    string result = (string)Clipboard.GetText(TextDataFormat.Text);
+                    Clipboard.Clear();
+                    sw.WriteLine("\n"+result);
+
+                    sw.Close();
+                }
             }
         }
     }
