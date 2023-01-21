dd2e71154485d0f593e893aefd92d7ac1ac0aa50
Stanislav Kaminskyi <slarchienko19@gmail.com>
2023-01-21 23:46:13 +0200

    Added draft UI general commands tools

---
+++ CommansBar.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DisEn
+{
+    /// <summary>
+    /// Interaction logic for CommansBar.xaml
+    /// </summary>
+    public partial class CommansBar : Window
+    {
+        public CommansBar()
+        {
+            InitializeComponent();
+        } 
+         //проверка на отмеченные команды и запись их в txt файл
+        public void CommandsCheck(object sender, ExecutedRoutedEventArgs e)
+        {
+            if(incCheckBox.IsChecked== true) { }
+        }
+    }
+}
