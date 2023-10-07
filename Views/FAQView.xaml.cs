0aef7fec03afdbdcb378cc9b361aa79fe2fcee9e
Stanislav Kaminskyi <slarchienko19@gmail.com>
2023-10-07 19:25:52 +0300

    Added simple version of F.A.Q.

--- Views/FAQView.xaml.cs
+++ Views/FAQView.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static DisEn.ViewModels.FAQViewModel;
 
 namespace DisEn.Views
 {
@@ -23,6 +24,8 @@ namespace DisEn.Views
         public FAQView()
         {
             InitializeComponent();
+
         }
+
     }
 }
